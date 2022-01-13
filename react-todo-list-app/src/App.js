32821c0bacc6030f153df2544d917e0ce9575c2e
ngannguyen0110 <nguyenthikimngan0110@gmail.com>
2022-01-13 20:26:38 +0900

    finish app

--- react-todo-list-app/src/App.js
+++ react-todo-list-app/src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="todo-app">
-      <hi>Todo App</hi>
+      <TodoList />
     </div>
   );
 }
